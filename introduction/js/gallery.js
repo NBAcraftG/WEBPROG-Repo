2e5360f60aa4e53e7726994b11da4f101b2babfb
Kurt Biñas <kcbinas@student.apc.edu.ph>
2026-01-01 19:12:58 +0800

    added img

--- introduction/js/gallery.js
+++ introduction/js/gallery.js
@@ -18,8 +18,8 @@ createApp({
         return {
           images: [
             'https://thirdimpactanime.com/wp-content/uploads/2018/04/wotakoi.png',
-            'https://wallpapers.com/images/hd/anime-city-night-4k-q3k6o3v6t8yk0z1r.jpg',
-            'https://wallpapers.com/images/hd/anime-scenery-4k-ultra-hd-9l5osn5y1x9ohx3z.jpg'
+            'https://cdn.wallpapersafari.com/60/15/XkY4aA.jpg',
+            'https://wallpapercave.com/wp/wp9597089.jpg'
           ]
         };
       }
